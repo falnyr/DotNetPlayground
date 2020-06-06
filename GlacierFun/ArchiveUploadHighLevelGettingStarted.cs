2836760098fada01b6a24c225f5a4c5dd84db0f6
Jan Alfred Richter <jan@pixelfusion.co.nz>
2020-06-07 00:23:49 +1200

    feat: following tutorial
    https://docs.aws.amazon.com/amazonglacier/latest/dev/getting-started-upload-archive-dotnet.html

---
+++ GlacierFun/ArchiveUploadHighLevelGettingStarted.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Amazon.Glacier;
+using Amazon.Glacier.Transfer;
+using Amazon.Runtime;
+
+namespace GlacierFun
+{
+    public class ArchiveUploadHighLevelGettingStarted
+    {
+        static string vaultName = "examplevault";
+        static string archiveToUpload = "*** Provide file name (with full path) to upload ***";
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var manager = new ArchiveTransferManager(Amazon.RegionEndpoint.USWest2);
+                // Upload an archive.
+                string archiveId = manager.Upload(vaultName, "getting started archive test", archiveToUpload).ArchiveId;
+                Console.WriteLine("Copy and save the following Archive ID for the next step.");
+                Console.WriteLine("Archive ID: {0}", archiveId);
+                Console.WriteLine("To continue, press Enter");
+                Console.ReadKey();
+            }
+            catch (AmazonGlacierException e) { Console.WriteLine(e.Message); }
+            catch (AmazonServiceException e) { Console.WriteLine(e.Message); }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+            Console.WriteLine("To continue, press Enter");
+            Console.ReadKey();
+        }
+    }
+}
